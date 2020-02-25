1c985682c768a9c560c00a97afde4ece4a75e309
Kevin <kevin.floba@gmail.com>
2020-02-25 17:03:51 -0300

    First Commit

---
+++ app/src/main/java/cl/desafiolatam/desafiopregunta/Questions.java
@@ -0,0 +1,22 @@
+package cl.desafiolatam.desafiopregunta;
+
+public class Questions {
+    private int response_code;
+    private QuestionsResults[] results;
+
+    public int getResponse_code() {
+        return this.response_code;
+    }
+
+    public void setResponse_code(int response_code) {
+        this.response_code = response_code;
+    }
+
+    public QuestionsResults[] getResults() {
+        return this.results;
+    }
+
+    public void setResults(QuestionsResults[] results) {
+        this.results = results;
+    }
+}
